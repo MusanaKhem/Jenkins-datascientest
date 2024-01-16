32c34f267c6f8622ed8ba387f62b2a4a223dd060
MusanaKhem <hermmann.matondo59@gmail.com>
2024-01-16 18:39:00 +0100

    Vingtième version, modification aap/main.py

--- app/main.py
+++ app/main.py
@@ -3,4 +3,4 @@ from fastapi import FastAPI
 app = FastAPI()
 @app.get("/")
 def read_root():
-    return {"Hello": "We Love Datascientest !!"}
+    return {"Hello": "We Love Datascientest. Very useful to use CI/CD !!"}
